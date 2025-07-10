f22f09d6277ea43f2f2c583ee17a655b833e3897
saya5140424 <tokunosima8008@gmail.com>
2025-07-10 09:44:38 +0900

    Newsの部分API取得OK

--- libs/client.js
+++
@@ -1,7 +0,0 @@
-// libs/client.js
-import { createClient } from 'microcms-js-sdk';
-
-export const client = createClient({
-  serviceDomain: 'ambassadeur',  
-  apiKey: process.env.API_KEY,      // process.env.API_KEY: APIキーの参照
-});
